e85b17139d02a01ae50461c85fd474f001416d9f
Thomas Vu <samduykhanh01@gmail.com>
2024-01-03 15:21:15 -0500

    Completed js exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(stringString) {
+    const stringArray = stringString.split("")
+    length = stringString.length
+    str = ""
+    for (let i = length - 1; i > -1; i--){
+        str += stringArray[i]
+    }
+    
+    return str
 
 };
 
