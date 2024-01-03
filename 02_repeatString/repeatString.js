a22cda975e7b9bc932d1d520a0db53684dd8b4db
Thomas Vu <samduykhanh01@gmail.com>
2024-01-03 14:40:56 -0500

    Completed js exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let stringReturn = ""
+    for (let i = 0; i < num; i++) {
+        stringReturn += `${i + 1}: ${string}\n`
+    }
+    return stringReturn
 };
 
 // Do not edit below this line
