361f6f58b2604c6b1c8879ce0d4fef808e84c971
Thomas Vu <samduykhanh01@gmail.com>
2024-01-03 20:41:03 -0500

    Completed js exercise 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,11 @@
 const reverseString = function(stringString) {
     const stringArray = stringString.split("")
     length = stringString.length
-    str = ""
+    let str = ""
     for (let i = length - 1; i > -1; i--){
         str += stringArray[i]
     }
-    
+
     return str
 
 };
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    let length = array.length;
+    if (length == 0) return [];
+    let newArray = [];
+    
+    /**
+     * Way 1:
+     * For each "item" in "array", if "item" is not in "elements",
+     *   then push that item into newArray
+     */
+    array.forEach(separate);
+    function separate(item) {
+        if (!elements.includes(item)) {
+            newArray.push(item);
+        }
+    }
 
+    /**
+     * Way 2:
+
+    for (const item of array) {
+        if (!elements.includes(item)) {
+            newArray.push(item)
+        }
+    }
+
+    */
+
+    return newArray
 };
 
 // Do not edit below this line
